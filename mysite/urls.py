4094e3c75ab8cb98c4d5cf0f6f81b05163526356
helltsy <helltsy@gmail.com>
2016-09-13 20:43:38 +0300

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
+from blog import views
 from django.contrib import admin
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
